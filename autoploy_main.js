644e63b7e9950abfecf90473bf35892e11e23749
Kartik Verma <3920286+vkartik97@users.noreply.github.com>
2018-03-11 03:18:15 +0530

    Github Ping Validation

--- autoploy_main.js
+++ autoploy_main.js
@@ -10,6 +10,11 @@ app.use(BodyParser.json());
 
 app.use('/deploy', deploy)
 
+// for Github Test Ping
+app.post('/', (req, res, next) => {
+    res.sendStatus(200)
+})
+
 const HTTPServer = app.listen(8010, (error) => {
     if(error) throw error
 })
