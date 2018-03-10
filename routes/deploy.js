a38ea589774b41f38df75a767198fe95b6e2d9a3
Kartik Verma <3920286+vkartik97@users.noreply.github.com>
2018-03-11 02:57:26 +0530

    Deployer v2

--- routes/deploy.js
+++ routes/deploy.js
@@ -2,78 +2,86 @@ const route = require('express').Router()
 
 const shell = require('shelljs')
 
-var projects = require('../config/projects')
+const projects = require('../config/projects')
 
 const fs = require('fs')
 
-var getUpdatedJSONData = (JSONFileName) => {
-    var file = fs.readFileSync(JSONFileName)
-    return JSON.parse(file)
-}
+const JSONHelper = require('../JSONHelper')
 
-var whereExist = (data, reference) => {
-    let array = Object.keys(reference)
-    for(var i = 0 ; i < array.length ; i++) {
-        if( data == array[i]) {
-            return i
-        }
+// To Stop current running project
+var stopRunCmds = (repoPath, req) => {
+    var data = JSONHelper.getUpdatedJSONData(repoPath +  "/autoploy.json")
+    var runCmds = data['run']
+    for(var i = 0 ; i < runCmds.length ; i++) {
+        shell.exec(`pkill -f "`+runCmds[i]+`"`)
     }
-    return -1
 }
 
-// Deployment happens here
-var deploy = (gitLink, repoPath, body, repo, res) => {
-
-    // checkout at latiest commit
-    shell.exec("git checkout "+body.after, {async:false})
-
-    // run
-    var deployer = getUpdatedJSONData(repoPath + "/autoploy.json")
-    var run = deployer.run
+var run = (gitLink, repoPath, req, data) => {
+    var runCmds = data['run']
     var i = 0
-    var recursion = () => {
-        shell.exec(run[0], {silent: true}, (code, stdout, stderr) => {
+    shell.cd(repoPath + "/" + req.body.repository.name)
+    let recursion = () => {
+        shell.exec(runCmds[i], {silent: false}, (code, stdout, stderr) => {
             if(code !== 0) {
-                return res.send(500)
+                console.log(stderr)
+                console.log(stdout)
             }
-            else
-            if(i<run.length) {
+            else if(i<runCmds.length) {
                 i++
                 recursion()
             }
         })
     }
     recursion()
+}
 
+var init = (gitLink, repoPath, req) => {
+    shell.cd(repoPath + "/" + req.body.repository.name)
+    if(fs.existsSync(repoPath + "/" + req.body.repository.name + "/autoploy.json")) {
+        var data = JSONHelper.getUpdatedJSONData(repoPath + "/" + req.body.repository.name + "/autoploy.json")
+        if(JSONHelper.whereExist("run", data) !== -1) {
+            run(gitLink, repoPath, req, data)
+        }
+    }
 }
 
 route.post('/', (req, res, next) => {
     // Check if Repo/Branch exist in projects.json
     let ref = req.body.ref.split('/') 
     let branch = ref[ref.length-1]
-    let repository = req.body.repository.name + "/" + branch
-    var ir  = whereExist(repository+"", projects)
-    if(ir !== -1) {
-        console.log(ir)
+    var repository = req.body.repository.name + "/" + branch
+    var ir  = JSONHelper.whereExist(repository, projects)
+    if(ir === -1) {
+        next()
     }
-    var gitLink = projects[repository][0]
-    var repoPath = projects[repository][1]
+    else if (req.body.repository.html_url === projects[repository][0]){
+        var gitLink = projects[repository][0]
+        var repoPath = projects[repository][1]
 
-    // Updating starts here
-    let repo = gitLink.ref.split('/')
-    repo = "git@github.com:/"+repo[repo.length-2] + "/" + repo[repo.length-1]
-    shell.exec("$ git clone "+repo)
-    shell.exec('cd ' +repoPath, {silent: true}, (code, stdout, stderr) => {
-        if(code === 0)
-        {
-            deploy(gitLink, repoPath, req.body, repo, res)
+        // To avoid Github's Timeout
+        res.sendStatus(200)
+
+        // Git starts here
+        let repo = gitLink.split('/')
+        repo = "git@github.com:/"+repo[repo.length-2] + "/" + repo[repo.length-1]
+        if(fs.existsSync(repoPath + "/" + req.body.repository.name)) {
+            shell.cd(repoPath + "/" + req.body.repository.name)
+            shell.exec("git pull")
+            stopRunCmds(repoPath + "/" + req.body.repository.name, req)
+            init(gitLink, repoPath, req)
         }
-        else
-        {
-            shell.exec("git clone "+repo)
+        else {
+            shell.cd(repoPath)
+            shell.exec("git clone " + repo)
+            init(gitLink, repoPath, req)
+            
         }
-    })
+    }
 })
 
+route.use((req, res, next) => {
+    // Waiting for timeout :)
+})
 
 module.exports = route
